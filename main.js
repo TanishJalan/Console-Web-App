17308f460919e1382250d4904c7ee0d09f3b3d3c
TanishJalan <108969446+TanishJalan@users.noreply.github.com>
2022-07-11 16:10:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var FName="Tanish";
+var LName="Jalan";
+var FuLLName=FName.concat(LName);
+console.log(FuLLName);
+var a=10;
+var b=5;
+var add= a + b;
+var sub= a - b;
+var mul= a * b;
+var div= a / b;
+console.log(add);
+console.log(sub);
+console.log(mul);
+console.log(div);
